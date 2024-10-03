2d36fb1fba38374a65adb0b67b9ecd29df3059db
IvanPoliakov <ivan.polyakoff2003@gmail.com>
2024-10-03 22:31:58 +0300

    change url

--- config/admin.ts
+++ config/admin.ts
@@ -1,4 +1,5 @@
 export default ({ env }) => ({
+  url: '/strapi',
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
--- config/server.ts
+++ config/server.ts
@@ -1,4 +1,8 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
+  url: 'http://209.126.9.253/strapi',
+  admin: {
+    url: '/strapi',
+  },
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
